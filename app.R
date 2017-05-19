f787611d919dcbd3b609893547d797c65fd31124
gcb63 <geoffbohling@me.com>
2017-05-19 09:11:03 -0500

    A few minor improvements

--- app.R
+++ app.R
@@ -16,8 +16,8 @@ ui <- fluidPage(
                   '.csv'
                 )
       ),
-      selectInput("xvar", "X coordinate: ", choices = NULL), 
-      selectInput("yvar", "Y coordinate: ", choices = NULL),
+      selectInput("xvar", "X coordinate: ", choices =NULL), 
+      selectInput("yvar", "Y coordinate: ", choices =NULL),
       selectInput("pvar", "Property: ", choices=NULL),
       numericInput("lagsp","Lag spacing: ", value=NULL),
       numericInput("maxlag","Maximum lag: ", value=NULL)
@@ -58,25 +58,28 @@ server <- function(input, output, session) { # added session for updateSelectInp
     f
   })
   
-  observeEvent( c(input$xvar,input$yvar,input$pvar), {
+  observeEvent( c(input$xvar,input$yvar), {
     if (!is.null(input$xvar) & input$xvar != "[NONE]" &
-        !is.null(input$yvar) & input$yvar != "[NONE]" &
-        !is.null(input$pvar) & input$pvar != "[NONE]") {
+        !is.null(input$yvar) & input$yvar != "[NONE]" ) {
       f <- info()
-      f <- subset(f, select = c(input$xvar,input$yvar,input$pvar))
+      f <- subset(f, select = c(input$xvar,input$yvar))
       f <- na.omit(f)
-      dlag <- median(nndist(f))
-      mlag <- 0.5*max(as.vector(dist(f)))
+      dlag <- signif(median(nndist(f)),2)
+      mlag <- signif(0.5*max(as.vector(dist(f))),2)
       updateNumericInput(session,"lagsp","Lag spacing: ",value=dlag)
       updateNumericInput(session,"maxlag","Maximum lag: ",value=mlag)
     }
   })
 
   output$postmap <- renderPlot({
-    if (!is.null(input$xvar) & input$xvar != "[NONE]" & !is.null(input$yvar) & input$yvar != "[NONE]" & !is.null(input$pvar) & input$pvar != "[NONE]" ) {
+    if (!is.null(input$xvar) & input$xvar != "[NONE]" &
+        !is.null(input$yvar) & input$yvar != "[NONE]" &
+        !is.null(input$pvar) & input$pvar != "[NONE]" ) {
       f <- info()
       f <- subset(f, select = c(input$xvar,input$yvar,input$pvar))
-      scatter2D(f[,1],f[,2],colvar=f[,3],pch=16,asp=1)
+      scatter2D(f[,1],f[,2],colvar=f[,3],pch=16,asp=1,cex=1.3,
+                xlab=input$xvar,ylab=input$yvar,cex.lab=1.3,
+                main=paste("Posting of",input$pvar),cex.main=1.3)
     }
   })
   
@@ -90,7 +93,11 @@ server <- function(input, output, session) { # added session for updateSelectInp
       f <- na.omit(f)
       coordinates(f) <- c(input$xvar,input$yvar)
       form <- formula(paste(input$pvar,"~1",sep=""))
-      plot(variogram(form,f,width=input$lagsp,cutoff=input$maxlag))
+      prop.vg <- variogram(form,f,width=input$lagsp,cutoff=input$maxlag)
+      with(prop.vg,
+           plot(gamma~dist,pch=16,col="blue",cex=1.3,ylim=c(0,1.2*max(prop.vg$gamma)),
+                xlab="Lag Distance",ylab="Semivariance",cex.lab=1.3,
+                main=paste("Omnidirectional Semivariogram of",input$pvar),cex.main=1.3))
     }
   })
   
